2a52b688c8a52ed6d6db58a7cb712a9298756261
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-09-17 10:12:03 +0000

    upate to routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from "@angular/router";
-<<<<<<< HEAD
-import { AppComponent } from "./app.component";
 
-export const routes: Routes = [];
-=======
 import { EmployeeComponent } from "./employee/employee.component";
 import { ClientComponent } from "./client/client.component";
 import { MasterComponent } from "./master/master.component";
@@ -28,4 +24,3 @@ export const routes: Routes = [
     component: ClientComponent,
   },
 ];
->>>>>>> c91b53600ee1c9b80b22d73f6047f4f1de20f33e
